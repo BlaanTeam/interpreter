c5a56bf899089bb6e844db080fb20339842bd1aa
Abdelhadi <abdo.sabani30@gmail.com>
2023-01-12 21:07:04 +0100

    chore: add global utils functions

---
+++ global_utils.cpp
@@ -0,0 +1,86 @@
+#include "interpreter.hpp"
+
+static void label(AST *node, int &id)
+{
+    if (!node)
+        return;
+
+    cout << "\tNode" << id << "[label=\"";
+
+    if (dynamic_cast<BinaryExpression *>(node))
+        cout << token_names[((BinaryExpression *)node)->op];
+    else if (dynamic_cast<UnarySign *>(node))
+        cout << "[" << ((UnarySign *)node)->sign << "]";
+    // else if (dynamic_cast<UnaryCommand *>(node))
+    //     cout << ((UnaryCommand *)node)->cmd;
+    else if (dynamic_cast<Number *>(node))
+        cout << ((Number *)node)->value;
+    else if (dynamic_cast<Identifier *>(node))
+        cout << ((Identifier *)node)->name;
+    else if (dynamic_cast<BinaryAssign *>(node))
+        cout << "=";
+
+    cout << "\"]\n";
+}
+
+static void edge(int from, int to)
+{
+    cout << "\tNode" << from << " -> "
+         << "Node" << to << "[wieght=9]\n";
+}
+
+static int dfs(AST *node, int &id)
+{
+    int my_id = 0, l, r;
+
+    my_id = id++;
+
+    if (node)
+    {
+        label(node, my_id);
+        if (dynamic_cast<Binary *>(node))
+        {
+            l = dfs(((Binary *)node)->left, id);
+            r = dfs(((Binary *)node)->right, id);
+            edge(my_id, l);
+            edge(my_id, r);
+        }
+        else if (dynamic_cast<Unary *>(node))
+        {
+            l = dfs(((Unary *)node)->expr, id);
+            edge(my_id, l);
+        }
+    }
+
+    return my_id;
+}
+
+void dump_dot(AST *tree)
+{
+    cout << "digraph {\n";
+    int id = 0;
+    dfs(tree, id);
+    cout << "}\n";
+}
+
+string get_number(const string &line, int &idx)
+{
+    int len = 0;
+    while (isdigit(line[idx]))
+    {
+        idx++;
+        len++;
+    }
+    return line.substr(idx - len, len);
+}
+
+string get_identifier(const string &line, int &idx)
+{
+    int len = 0;
+    while (isalnum(line[idx]) || line[idx] == '_')
+    {
+        len++;
+        idx++;
+    }
+    return line.substr(idx - len, len);
+}
---
+++ global_utils.hpp
@@ -0,0 +1,16 @@
+#ifndef __GLOBAL_UTILS_H__
+#define __GLOBAL_UTILS_H__
+
+#include <string>
+#include "ast.hpp"
+
+using namespace std;
+
+// global utils prototypes
+
+string get_number(const string &line, int &idx);
+string get_identifier(const string &line, int &idx);
+
+void dump_dot(AST *);
+
+#endif
